d785ca0e7b4d69e9622bdf8b46815dce698be40a
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-26 07:32:53 +0900

    feat: section02 실습

---
+++ src/gaeun/section02/ex/MovieReview.java
@@ -0,0 +1,7 @@
+package gaeun.section02.ex;
+
+public class MovieReview {
+    String title;
+    String review;
+
+}
---
+++ src/gaeun/section02/ex/ProductOrder.java
@@ -0,0 +1,7 @@
+package gaeun.section02.ex;
+
+public class ProductOrder {
+    String productName;
+    int price;
+    int quantity;
+}
