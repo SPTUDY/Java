2d413ca1f070c8cbf45360644e3663a0bb3a2b81
gaeunnlee <gaeunnnleee@gmail.com>
2025-07-14 03:18:36 +0900

    feat: section08 실습

---
+++ src/gaeun/section08/static2/DecoData.java
@@ -0,0 +1,37 @@
+package gaeun.section08.static2;
+
+public class DecoData {
+    private int instanceValue;
+    private static int staticValue;
+
+    // 정적 메서드로, 정적 메서드와 정적 변수만 접근 가능
+    public static void staticCall () {
+//        instanceValue ++; // 인스턴스 변수 접근, compile error
+//        instanceMethod(); // 인스턴스 메서드 접근, compile error
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    public static void staticCall(DecoData data) {
+        data.instanceValue++;
+        data.instanceMethod();
+    }
+
+    public void instanceCall () {
+        instanceValue++; // 인스턴스 변수 접근
+        instanceMethod(); // 인스턴스 메서드 접근
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    private void instanceMethod () {
+        System.out.println("instanceValue: " + instanceValue);
+    }
+
+
+    private static void staticMethod () {
+        System.out.println("staticValue: " + staticValue);
+    }
+}
