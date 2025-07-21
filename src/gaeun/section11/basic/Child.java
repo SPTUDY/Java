6da6e8dd8bdf7ec300e9066639b7cd2017e1f9f6
gaeunnlee <gaeunnnleee@gmail.com>
2025-07-22 01:37:21 +0900

    feat: section11 실습

---
+++ src/gaeun/section11/basic/Child.java
@@ -0,0 +1,7 @@
+package gaeun.section11.basic;
+
+public class Child extends Parent {
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
---
+++ src/gaeun/section11/overriding/Parent.java
@@ -0,0 +1,11 @@
+package gaeun.section11.overriding;
+
+public class Parent {
+
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent.method");
+    }
+}
+
